5bdad44c2ef327e83ee2fe4dd9fd167b2c2bd548
charyung <charmaineyung@yahoo.com.hk>
2020-07-07 21:52:34 -0400

    Updates

---
+++ precache-manifest.15d96d4ada4c33fda1a6dce6442298f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e607711568f537d3155cf7691376f6",
+    "url": "/blaise-transit-fall-2020/index.html"
+  },
+  {
+    "revision": "44bfd00f80cf923b05c5",
+    "url": "/blaise-transit-fall-2020/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "8cd4b82e1f21de751ee2",
+    "url": "/blaise-transit-fall-2020/static/css/main.47eec2ba.chunk.css"
+  },
+  {
+    "revision": "44bfd00f80cf923b05c5",
+    "url": "/blaise-transit-fall-2020/static/js/2.3553c027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blaise-transit-fall-2020/static/js/2.3553c027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd4b82e1f21de751ee2",
+    "url": "/blaise-transit-fall-2020/static/js/main.d5986430.chunk.js"
+  },
+  {
+    "revision": "439794dbecfae63fad6a",
+    "url": "/blaise-transit-fall-2020/static/js/runtime-main.37dc6eba.js"
+  }
+]);
